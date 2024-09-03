99fd192b825fbf7f77d96fe19c1c9fa6bfd801d2
jianjio <jtiotangco3@gmail.com>
2024-09-03 16:54:51 +0800

    MP2 Nieves & Tiotangco

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-slate-800 sticky bottom-0">
+    <footer className="bg-slate-800 justify-between">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2024{" "}
--- src/Component/Header.js
+++ src/Component/Header.js
@@ -4,19 +4,14 @@ import Background from "../assets/pxfuel.jpg";
 
 const Header = () => {
   return (
-    <nav className="bg-[#53212B] sticky top-0">
+    <nav className="bg-slate-800 sticky top-0">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4">
         <a href="#" className="flex items-center rtl:space-x-reverse">
           <img src={Image} className="h-20" alt="Gamedev" />
           <span className="self-center text-5xl font-semibold whitespace-nowrap text-white">
-            DevGames
+            DefinitelyFreeGames
           </span>
         </a>
-        {/* <img
-          className="bg-repeat-x opacity-20"
-          src={Background}
-          alt="Valorant Image"
-        /> */}
       </div>
     </nav>
   );
