3a297bef56e4fb06fc2178e540cbe67a334db31c
jianjio <jtiotangco3@gmail.com>
2024-09-10 21:59:06 +0800

    MP2 Nieves & Tiotangco

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import "./App.css";
-import Carousel from "./Component/Carousel";
+
 import Header from "./Component/Header";
 import Footer from "./Component/Footer";
 import Api from "./Component/Api";
-import Modal from "./Component/Modal";
 
 function App() {
   return (
